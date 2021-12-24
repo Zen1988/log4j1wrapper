4cdc864e420f242b3c56a1b01c07196d63384ee8
Zen1988 <yunnchyuanchan@gmail.com>
2021-12-24 22:06:27 +0800

    Initial upload
    Initial upload

---
+++ src/main/java/org/apache/log4j/LayoutWrapper.java
@@ -0,0 +1,35 @@
+/*
+ * 
+ */
+package org.apache.log4j;
+
+/**
+ * This class is to convert log4j 1.x Layout object to 2.x Layout object
+ *
+ * @author Zen
+ * @version 1.0
+ */
+public class LayoutWrapper {
+
+    /**
+     * This function will convert layout from log4j 1.x to log4j 2.x
+     *
+     * @param layout - org.apache.log4j.Layout
+     * @return org.apache.logging.log4j.core.Layout
+     */
+    public static org.apache.logging.log4j.core.Layout layout1to2(
+            org.apache.log4j.Layout layout) {
+        if (layout.getClass().getSimpleName().toLowerCase()
+                .equals("patternlayout")) {
+            PatternLayout pLayout = (PatternLayout) layout;
+            org.apache.logging.log4j.core.layout.PatternLayout pLayout2
+                    = org.apache.logging.log4j.core.layout.PatternLayout
+                            .newBuilder()
+                            .withPattern(pLayout.getConversionPattern())
+                            .build();
+            return pLayout2;
+        }
+        return org.apache.logging.log4j.core.layout.PatternLayout
+                .createDefaultLayout();
+    }
+}
